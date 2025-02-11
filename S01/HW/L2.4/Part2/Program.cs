4b1ab1206f1ed373a80b34df576d25300e28aa69
Negar <negarerezaei@gmail.com>
2025-02-11 19:33:11 +0330

    add L2.4

---
+++ S01/HW/L2.4/Part2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Part2;
+
+class Program
+{
+    public static void print_left_triangle(int baseLength, char character)
+    {
+        if(character == '\0')
+            character = '*';
+
+        for(int i=1; i<baseLength+1; i++)
+        {
+            for(int j=1; j<i+1; j++)
+                Console.Write(character);
+            Console.WriteLine();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        print_left_triangle(20,'%');
+    }
+}
