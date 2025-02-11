5be022e236f8f5d0fbf4aa79091d412f564e3b98
Negar <negarerezaei@gmail.com>
2025-02-11 23:47:30 +0330

    add L2.6

---
+++ S01/HW/L2.6/Part7/Pythagorean.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Part7;
+
+class Program
+{
+    public static bool Pythagorean_Triples(int a ,int b ,int c)
+    {
+        if(a*a + b*b == c*c)
+            return true;
+        return false;
+    }
+
+    static void Main(string[] args)
+    {
+        for(int i = 1; i<100; i++){
+            for(int j = i+1; j<100; j++){
+                for(int k = j+1; k<100; k++){
+                    if(Pythagorean_Triples(i, j, k)){
+                        Console.WriteLine("a = " + i + "    b = " + j + "    c = " + k);
+                    }
+                }
+            }
+        }
+    }
+}
