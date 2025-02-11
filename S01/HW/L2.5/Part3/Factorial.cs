8e599e90091ab891d2a5bc12cfca30cc8120ef4b
Negar <negarerezaei@gmail.com>
2025-02-11 20:41:37 +0330

    add L2.5

---
+++ S01/HW/L2.5/Part3/Factorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Part3;
+
+class Program
+{
+    public static ulong factorial(int n)
+    {
+        ulong number = 1;
+        while(n>0)
+        {
+            number *= (ulong)n;
+            n--;
+        }
+        return number;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(factorial(10));
+    }
+}
