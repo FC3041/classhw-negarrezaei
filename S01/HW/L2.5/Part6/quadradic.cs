8481ece0301efab0857f6d0c8291c15cafb9825a
Negar <negarerezaei@gmail.com>
2025-02-11 20:47:06 +0330

    add L2.5

---
+++ S01/HW/L2.5/Part6/quadradic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Part6;
+
+class Program
+{
+    public static void quadradic(float A, float B, float C)
+    {
+        float Delta = (float)(Math.Pow(B,2) - 4*A*C);
+        if(Delta > 0)
+        {
+            float x1 = (float)((-B + Math.Sqrt(Delta)) / (2 * A));
+            float x2 = (float)((-B - Math.Sqrt(Delta)) / (2 * A));
+            Console.WriteLine("x1 = " + x1);
+            Console.WriteLine("x2 = " + x2);
+        }
+        else if(Delta == 0)
+        {
+            float x = (float)((-B + Math.Sqrt(Delta)) / (2 * A));
+            Console.WriteLine("x = " + x);
+        }
+        else
+            Console.WriteLine("None");
+    }
+    static void Main(string[] args)
+    {
+        quadradic(1,2,3);
+        quadradic(1,-4,3);
+    }
+}
