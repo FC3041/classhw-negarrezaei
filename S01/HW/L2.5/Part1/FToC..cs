af47933ba8e503b985acff3549820fcd7c534ab6
Negar <negarerezaei@gmail.com>
2025-02-11 20:37:36 +0330

    add L2.5

---
+++ S01/HW/L2.5/Part1/FToC..cs
@@ -0,0 +1,16 @@
+﻿namespace Part1;
+
+class Program
+{
+    public static void temperature(float F)
+    {
+        float C = (F - 32) * 5/9;
+        Console.WriteLine("{0:0.00}",F);
+        Console.WriteLine("{0:0.00}",C);
+    }
+
+    static void Main(string[] args)
+    {
+        temperature(50.8f);
+    }
+}
