c4384ab7367399ff45f09806cbf827381ce697f3
Negar <negarerezaei@gmail.com>
2025-02-11 23:45:28 +0330

    add L2.6

---
+++ S01/HW/L2.6/Part6/ComputeChange.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Part6;
+
+class Program
+{
+    public static void compute_change(int cents)
+    {
+        cents = 100 - cents;
+        int quarters = cents / 25;
+        cents %= 25;
+        Console.WriteLine(quarters + " quarters");
+
+        int dime = cents / 10;
+        cents %= 10;
+        Console.WriteLine(dime + " dime");
+
+        int nickel = cents / 5;
+        cents %= 5;
+        Console.WriteLine(nickel + " nickel");
+
+        int pennies = cents;
+        Console.WriteLine(pennies + " pennies");
+    }
+
+    static void Main(string[] args)
+    {
+        compute_change(8);
+    }
+}
