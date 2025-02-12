5269d1dad7ae33c5d184603b7bc5a024c39b22a7
Negar <negarerezaei@gmail.com>
2025-02-12 22:51:34 +0330

    add L2.7

---
+++ S01/HW/L2.7/Part2/factorial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Part2;
+
+class Program
+{
+    public static ulong Factorial(int n)
+    {
+        ulong number = 1;
+        for(int i=1; i<n+1; i++)
+            number *= (ulong)i;
+        return number;
+    }
+
+    static void Main(string[] args)
+    {
+        for(int j=1; j<21; j++)
+            Console.WriteLine(j + "! = " + Factorial(j));
+    }
+}
