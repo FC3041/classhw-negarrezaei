7300b59f3e6165bd85a63e26d4c90e182e557052
Negar <negarerezaei@gmail.com>
2025-02-13 13:40:47 +0330

    add L2.10

---
+++ S01/HW/L2.10/Part3A/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Part3A;
+
+class Program
+{
+    public static float Abs(float x)
+    {
+        if(x >= 0)
+            return x;
+        return -x; 
+    }
+    public static bool near(float x, float y, double closeness)
+    {
+        float x1 = Abs(x);
+        float y1 = Abs(y);
+        if((y1 > (1 - closeness) * x1) && (y1 < (1 + closeness) * x1))
+            return true;
+        return false;
+    }
+
+    static void Main(string[] args)
+    {
+        if(near(102,102.1f, 0.001))
+            Console.WriteLine("True");
+        else
+            Console.WriteLine("False");
+    }
+}
