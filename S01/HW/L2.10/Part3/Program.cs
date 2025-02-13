76d8a87fca8eec62d406ecfac3abd331f1838126
Negar <negarerezaei@gmail.com>
2025-02-13 13:39:25 +0330

    add L2.10

---
+++ S01/HW/L2.10/Part3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Part3;
+
+class Program
+{
+    public static double exp2(int x, double precision)
+    {
+        int N = 1;
+        int n = 0;
+        double a  = (double) Math.Pow(x,n)/N; 
+        double e = 0.0;
+        while(a > precision)
+        {
+            a  = (double) Math.Pow(x,n)/N;
+            n++;
+            N *= n;
+            e += a;   
+        }
+        return e;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(exp2(1 , 0.000001));
+        Console.WriteLine(exp2(2 , 0.000001));
+    }
+}
