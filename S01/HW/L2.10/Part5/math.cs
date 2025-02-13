a0db6ef92a4e3e58f7f174d5123148b02f0e0ee9
Negar <negarerezaei@gmail.com>
2025-02-13 13:44:55 +0330

    add L2.10

---
+++ S01/HW/L2.10/Part5/math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Part5;
+
+class Program
+{
+    public static double square_root(double x, double precision)
+    {
+        double lowerBound = 0;
+        double upperBound = x;;
+        int iterations = 0;
+        double mid = 0;
+        while(upperBound - lowerBound > precision)
+        {
+            mid = (upperBound + lowerBound) / 2;
+            if(Math.Pow(mid,2) > x)
+                upperBound = mid;
+            else
+                lowerBound = mid;
+            iterations++;
+        }
+        Console.WriteLine("iterations = "+ iterations);
+        return mid;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(square_root(2, 0.000001));
+        Console.WriteLine(Math.Pow(2,0.5));
+    }
+}
