7a8c73c9ff3bcbc8b160cebd85e2c2624b4f496d
Negar <negarerezaei@gmail.com>
2025-02-13 13:36:21 +0330

    add L2.10

---
+++ S01/HW/L2.10/Part1/math.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Part1;
+
+class Program
+{
+    public static ulong factorial(int n)
+    {
+        if(n == 0)
+            return 1;
+        ulong number = 1;
+        while(n > 0){
+            number *= (ulong)n;
+            n--;
+        }
+        return number;
+    }
+
+    public static double eulers_constant(double precision)
+    {
+        int n = 1;
+        double a = 1.0/factorial(n);
+        double e = 1.0;
+        while (a > precision)
+        {
+            a = 1.0/factorial(n);
+            n++;
+            e += a;
+        }
+       
+        return e; 
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(eulers_constant(0.0001));
+    }
+}
